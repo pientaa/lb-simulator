1158bb5d3f32f12a6a5de91555c172b645568560
pafffko <kaciczak.p@gmail.com>
2020-10-20 13:32:03 +0200

    loads_save_tasks
    Generate vectors of loads (values from normal distribution), save to file (*.csv), count tasks in deltaT.

--- generator/generator.py
+++ generator/generator.py
@@ -1,31 +1,35 @@
-#import numpy
+import numpy as np
 import sys
 import csv
 import random
+import matplotlib.pyplot as plt
+import scipy.stats
 
 def generator(shards, deltaT, N, mean, std):
-    tmp_list = []
     vectors = []
+    tasks_in_deltaT = []
 
-    for x in range(shards):
-        for y in range(N):
-            tmp_list.append(random.randint(0, 10))
-        vectors.append(tmp_list)
-        tmp_list = []
-
+    for z in range(shards):        
+        y = np.random.normal(mean, std, N).tolist()
+        vectors.append(y)
+        y = []
 
     vectors_file = open("./generator/vectors.csv","w")
-    for x in vectors:
-        vector_string = str(vectors.index(x)+1) +","+ ','.join(map(str, x)) + "\n"
-        vectors_file.write(vector_string)
-
-
-
-
-
+    for z in vectors:
+       vector_string = str(vectors.index(z)+1) +","+ ','.join(map(str, z)) + "\n"
+       vectors_file.write(vector_string)
+    vectors_file.close()
+        
     
+    for n in range(N):
+        tmp = 0
+        for v in vectors:
+            if(v[n]>0):
+                tmp = tmp + 1
+        tasks_in_deltaT.append(tmp)
+        
+        
 
-    
 
 if __name__== "__main__":
     generator(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
