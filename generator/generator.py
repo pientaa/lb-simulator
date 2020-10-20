8bc61c00dd6cee880823061b56f358ce8f0cbd6f
pafffko <kaciczak.p@gmail.com>
2020-10-20 17:19:00 +0200

    Changed for Lucas
    Create functions, changed names of variables. Better encapsulation.

--- generator/generator.py
+++ generator/generator.py
@@ -1,64 +1,89 @@
 import numpy as np
 import sys
-import csv
 import random
-import matplotlib.pyplot as plt
-import scipy.stats
 
-def generator(shards, deltaT, N, mean, std):
+def generator(num_of_shards, delta_t, size_of_vectors, mean, std):
     vectors = []
-    tasks_in_deltaT = []
+    tasks_in_delta_t = []
+    time_marks_in_delta_t = []
     requests = []
-    time_marks_in_deltaT = []
 
-    for z in range(shards):        
-        y = np.random.normal(mean, std, N).tolist()
-        y = [round(num, 3) for num in y]
-        vectors.append(y)
-        y = []
+    vectors = create_vectors(int(num_of_shards), int(size_of_vectors), float(mean), float(std))
 
+    save_vectors(vectors)
+            
+    tasks_in_delta_t = count_positive_tasks(int(size_of_vectors), vectors)
+
+    time_marks_in_delta_t = generate_time_stamps(int(size_of_vectors), int(delta_t), tasks_in_delta_t)
+    
+    requests = create_requests(int(size_of_vectors), tasks_in_delta_t, time_marks_in_delta_t, vectors)
+
+    requests_sorted = sorted(requests, key=lambda x: x[0])
+    
+    add_indexes_to_requests(requests_sorted)
+
+    save_requests(requests_sorted)
+
+def create_vectors(num_of_shards, size_of_vectors, mean, std):
+    vectors = []
+    for shard in range(num_of_shards): 
+        loads = []    
+        loads = np.random.normal(mean, std, size_of_vectors).tolist()
+        loads = [round(load, 3) for load in loads]
+        vectors.append(loads)
+    return vectors
+
+def save_vectors(vectors):
     vectors_file = open("./generator/vectors.csv","w")
-    for z in vectors:
-       vector_string = str(vectors.index(z)+1) +","+ ','.join(map(str, z)) + "\n"
+    for vector in vectors:
+       vector_string = str(vectors.index(vector)+1) +","+ ','.join(map(str, vector)) + "\n"
        vectors_file.write(vector_string)
     vectors_file.close()
-            
-    for n in range(N):
-        tmp = 0
-        for v in vectors:
-            if(v[n]>0):
-                tmp = tmp + 1
-        tasks_in_deltaT.append(tmp)
 
+def count_positive_tasks(size_of_vectors, vectors):
+    tasks_in_delta_t = []
+    for column in range(size_of_vectors):
+        num_of_positive_loads = 0
+        for vector in vectors:
+            if(vector[column]>0):
+                num_of_positive_loads = num_of_positive_loads + 1
+        tasks_in_delta_t.append(num_of_positive_loads)
+    return tasks_in_delta_t
 
-    for n in range(N):
-        tmp_deltaT_start = n * deltaT
-        tmp_deltaT_end = (n + 1) * deltaT
-        time_marks = np.random.uniform(tmp_deltaT_start, tmp_deltaT_end, tasks_in_deltaT[n])
-        time_marks = [round(tm, 2) for tm in time_marks]
-        time_marks_in_deltaT.append(time_marks)
+def generate_time_stamps(size_of_vectors, delta_t, tasks_in_delta_t):
+    time_marks_in_delta_t = []
+    for size_of_vector in range(size_of_vectors):
+        delta_t_start = size_of_vector * delta_t
+        delta_t_end = (size_of_vector + 1) * delta_t
+        time_marks = np.random.uniform(delta_t_start, delta_t_end, tasks_in_delta_t[size_of_vector])
+        time_marks = [round(time_mark, 2) for time_mark in time_marks]
+        time_marks_in_delta_t.append(time_marks)
+    return time_marks_in_delta_t
 
-    tmp_req = []
-    for n in range(N):
-        task_iterator = tasks_in_deltaT[n] - 1
-        for v in vectors:
-            if(v[n]>0):
-                tmp_req.append(time_marks_in_deltaT[n][task_iterator])
-                tmp_req.append(vectors.index(v)+1)
-                tmp_req.append(v[n])
+def create_requests(size_of_vectors, tasks_in_delta_t, time_marks_in_delta_t, vectors):
+    requests = []
+    for size_of_vector in range(size_of_vectors):
+        task_iterator = tasks_in_delta_t[size_of_vector] - 1
+        for vector in vectors:
+            if(vector[size_of_vector]>0):
+                tmp_req = []
+                tmp_req.append(time_marks_in_delta_t[size_of_vector][task_iterator])
+                tmp_req.append(vectors.index(vector)+1)
+                tmp_req.append(vector[size_of_vector])
                 task_iterator = task_iterator - 1
                 requests.append(tmp_req)
                 tmp_req = []
-    
-    requests_sorted = sorted(requests, key=lambda x: x[0])
-    
-    for x in requests_sorted:
-        x.insert(0, requests_sorted.index(x) + 1)
+    return requests
 
+def add_indexes_to_requests(requests_sorted):
+    for request in requests_sorted:
+        request.insert(0, requests_sorted.index(request) + 1)
+
+def save_requests(requests_sorted):
     requests_file = open("./generator/requests.csv","w")
     requests_file.write("id, timestamp, shard, size \n")
-    for x in requests_sorted:
-        request_string = ','.join(map(str, x)) + "\n"
+    for request in requests_sorted:
+        request_string = ','.join(map(str, request)) + "\n"
         requests_file.write(request_string)
     requests_file.close()
 
